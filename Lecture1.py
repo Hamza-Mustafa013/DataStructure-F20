a63281328bcbb366b2a37f7bb6072faa5bf187a6
Hamza-Mustafa013 <bm234045@gmail.com>
2020-08-29 15:23:55 +0500

    In this lecture wewill learn with stack And use FIFO

--- Lecture1.py
+++ Lecture1.py
@@ -1,6 +1,6 @@
 #In this lecture we will learn Stack.. A form of Database Structure.... LIFO means Last In First Out
 #Means jo cheez sab sa pehla enter ho gi wo sab sa last pa bhr aye gi...Example below you can see
-#or is k 2 process hain push or pop
+
 
 #
 list=[]
