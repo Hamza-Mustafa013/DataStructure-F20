bf3ad82de63b3f43137611496865fde8d460f3ce
Hamza-Mustafa013 <bm234045@gmail.com>
2020-09-02 22:12:46 +0500

    Algorithm of selection sort

---
+++ Assignment 1.py
@@ -0,0 +1,13 @@
+def sorting(list):
+    counter1 = 0
+    while counter1 < len(list):
+        counter2 = counter1 + 1
+        while counter2 < len(list):
+            if list[counter1] > list[counter2]:
+                temp = list[counter1]
+                list[counter1] = list[counter2]
+                list[counter2] = temp
+            counter2 = counter2 + 1
+        counter1 = counter1 + 1
+    print(list)
+sorting([7,5,4,9,3,1])
