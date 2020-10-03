f7731b0781b3b3a603b2efd548199b0301983d02
Hamza-Mustafa013 <bm234045@gmail.com>
2020-10-04 02:04:14 +0500

    Algorithm of Selection Sort

---
+++ Selection sort.py
@@ -0,0 +1,12 @@
+list = [4,9,5,2,3,1]
+counter1 = 0
+while counter1 < len(list):
+    counter2 = counter1 + 1
+    while counter2 < len(list):
+        if list[counter1] > list[counter2]:
+            temp = list[counter1]
+            list[counter1] = list[counter2]
+            list[counter2] = temp
+        counter2 = counter2 + 1
+    counter1 = counter1 + 1
+print(list)
